4952e113627a88b044bdcfa1079ef5c3753c7118
fergus <fergus2121@outlook.com>
2019-06-04 16:39:16 +0800

    release v1.0.7

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
 importScripts(
-  "/precache-manifest.8a7cf45a7ea14fa5f4556148514ab104.js"
+  "/precache-manifest.a65d340ec3a4caac00e6dc64f317da38.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "smartFund"});
